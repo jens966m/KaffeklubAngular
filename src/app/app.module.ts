5ac8ff429705a49b9dd0bd284b3bb79ee418092a
Jens <jensdideriksen1990@gmail.com>
2018-11-11 21:20:34 +0100

    angularAppFirstCommit

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -3,10 +3,16 @@ import { NgModule } from '@angular/core';
 
 import { AppRoutingModule } from './app-routing.module';
 import { AppComponent } from './app.component';
+import { NavbarComponent } from './shared/navbar/navbar.component';
+import { MembersListComponent } from './members/members-list/members-list.component';
+
 
 @NgModule({
   declarations: [
-    AppComponent
+    AppComponent,
+    NavbarComponent,
+    MembersListComponent,
+    Member
   ],
   imports: [
     BrowserModule,
